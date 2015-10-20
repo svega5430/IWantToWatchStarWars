8d1b4d6076ef6bcd46353705878f52e8d3d3086e
svega5430 <svega5430@001LAB153010807.neisd.ad>
2015-10-20 15:47:16 -0500

    :D

---
+++ IWantToWatchTheStarWarsTrailer/src/IWantToWatchTheStarWarsTrailer.java
@@ -0,0 +1,9 @@
+
+public class IWantToWatchTheStarWarsTrailer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("The Star Wars Trailer was good.");
+	}
+
+}
